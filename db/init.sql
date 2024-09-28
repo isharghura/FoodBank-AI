ab91fbbfbb5da4ab59e16103d3fdae0db19c3084
isharghura <ishar.ghuras@gmail.com>
2024-09-28 12:54:00 -0400

    Updated users, foods, points

---
+++ db/init.sql
@@ -0,0 +1,21 @@
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+	name varchar(255) not null
+);
+
+CREATE TABLE foods (
+    food_id SERIAL PRIMARY KEY,
+    food_name VARCHAR(255) NOT NULL,
+	expiry TIMESTAMP NOT NULL,
+	nutrition_info JSONB,
+	point_val INT,
+    quantity INT DEFAULT 0,
+    
+);
+
+CREATE TABLE points (
+    point_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id),
+    food_id INT REFERENCES foods(food_id),
+    time_submitted TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
